7f1b0157a0c2998221926c8c1d4da3d7d421759d
Salma158 <salmasherif856@gmail.com>
2024-04-29 04:05:16 +0200

    Added basic navbar

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomeWrapperComponent } from './pages/home/home-wrapper/home-wrapper.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', title: 'home', component: HomeWrapperComponent}
+];
---
+++ front-end/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavContainer } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterOutlet,MatSidenavModule, MatSidenavContainer, MatListModule, MatIconModule, MatToolbarModule, MatMenuModule, FlexLayoutModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
