56f3a9797c42822a25757da475485b754d2cdeb1
Salma158 <salmasherif856@gmail.com>
2024-04-30 01:56:45 +0200

    added basic menu items table in admin dashboard

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeWrapperComponent } from './pages/home/home-wrapper/home-wrapper.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
+import { MenuItemsComponent } from './pages/admin/menu-items/menu-items.component';
 
 export const routes: Routes = [
     {path: '', title: 'home', component: HomeWrapperComponent},
-    {path: 'menu', title: 'menu', component: MenuComponent}
+    {path: 'menu', title: 'menu', component: MenuComponent},
+    {
+        path: 'admin',
+        component: AdminDashboardComponent,
+        children: [
+            { path: 'menuitems', title: 'admin-menuItems' , component: MenuItemsComponent }
+        ]
+    },
 ];
