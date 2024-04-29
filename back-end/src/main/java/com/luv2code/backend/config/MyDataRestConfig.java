bc4b5bf51cec15f1d29ed05ed6ee86dbe7adc1ab
Salma158 <salmasherif856@gmail.com>
2024-04-29 22:20:03 +0200

    Disallowed put , post , delete http methods

---
+++ back-end/src/main/java/com/luv2code/backend/config/MyDataRestConfig.java
@@ -0,0 +1,30 @@
+package com.luv2code.backend.config;
+
+import com.luv2code.backend.entity.Category;
+import com.luv2code.backend.entity.MenuItem;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST , HttpMethod.DELETE };
+
+        config.getExposureConfiguration()
+                .forDomainType(MenuItem.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        config.getExposureConfiguration()
+                .forDomainType(Category.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+    }
+}
