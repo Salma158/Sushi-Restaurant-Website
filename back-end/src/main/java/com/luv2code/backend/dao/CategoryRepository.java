5d815308770c8b082d97214cb9753b75db56fd7f
Salma158 <salmasherif856@gmail.com>
2024-04-30 18:43:07 +0200

    Modifed rest configuration to expose ids

--- back-end/src/main/java/com/luv2code/backend/dao/CategoryRepository.java
+++ back-end/src/main/java/com/luv2code/backend/dao/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.backend.dao;
 import com.luv2code.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "category", path = "menu-item-category")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- back-end/src/main/java/com/luv2code/backend/dao/MenuItemRepository.java
+++ back-end/src/main/java/com/luv2code/backend/dao/MenuItemRepository.java
@@ -2,6 +2,8 @@ package com.luv2code.backend.dao;
 
 import com.luv2code.backend.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 }
