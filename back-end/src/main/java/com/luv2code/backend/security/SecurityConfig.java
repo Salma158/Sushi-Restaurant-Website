d46363e0532f32e4d1bed96bc42e7d0072cb5186
Salma158 <salmasherif856@gmail.com>
2024-05-04 03:14:27 +0200

    Added cloudinary configuration

--- back-end/src/main/java/com/luv2code/backend/security/SecurityConfig.java
+++ back-end/src/main/java/com/luv2code/backend/security/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(
-                                        "api/auth/**"
+                                        "api/auth/**",
+                                        "api/menuItems"
                                 )
                                     .permitAll()
                                 .anyRequest()
