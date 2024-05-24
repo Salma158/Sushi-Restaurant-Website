6752d6f6bca71cb2bbc9ca3322ca108f39b34fd9
Salma158 <salmasherif856@gmail.com>
2024-05-24 17:43:09 +0200

    added why us component and styled it

--- front-end/src/app/pages/home/home-wrapper/home-wrapper.component.ts
+++ front-end/src/app/pages/home/home-wrapper/home-wrapper.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { WhyUsComponent } from '../why-us/why-us.component';
 
 @Component({
   selector: 'app-home-wrapper',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, WhyUsComponent],
   templateUrl: './home-wrapper.component.html',
   styleUrl: './home-wrapper.component.scss'
 })
