d4f645e93720dedb6302e4440c0f55bea2d9ee45
Salma158 <salmasherif856@gmail.com>
2024-05-24 19:43:29 +0200

    Added and styled contact us page

--- front-end/src/app/pages/contact/contact.component.ts
+++ front-end/src/app/pages/contact/contact.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,MatCardModule, MatFormFieldModule, FlexLayoutModule, MatIconModule,MatButtonModule,MatInputModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
