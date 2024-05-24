abc2e183ec64ff83378e46087928652dda813810
Salma158 <salmasherif856@gmail.com>
2024-05-24 20:31:45 +0200

    Integrated email js to handle contact us form

--- front-end/src/app/pages/contact/contact.component.ts
+++ front-end/src/app/pages/contact/contact.component.ts
@@ -6,6 +6,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
@@ -14,6 +15,15 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
 
+export class ContactComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_rfl4ppf', 'template_2lzu81b', e.target as HTMLFormElement, 'yONj_iUijuDbjAxPm')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 }
