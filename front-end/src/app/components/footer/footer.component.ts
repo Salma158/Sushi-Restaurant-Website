9e7003567162fc4891cd5b752072ff79d29baf1f
Salma158 <salmasherif856@gmail.com>
2024-05-25 00:38:40 +0200

    styled about us page

--- front-end/src/app/components/footer/footer.component.ts
+++ front-end/src/app/components/footer/footer.component.ts
@@ -3,10 +3,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatListModule, FlexLayoutModule],
+  imports: [MatIconModule, MatButtonModule, MatListModule, FlexLayoutModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
--- front-end/src/app/pages/about/about.component.ts
+++ front-end/src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, FlexLayoutModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
