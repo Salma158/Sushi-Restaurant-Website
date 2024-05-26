c76da28dd4526a829eb63ed446ac472cef4c2887
Salma158 <salmasherif856@gmail.com>
2024-05-26 03:47:47 +0200

    styled activation template

--- front-end/src/app/pages/activate-account/activate-account.component.ts
+++ front-end/src/app/pages/activate-account/activate-account.component.ts
@@ -3,27 +3,49 @@ import { AuthServiceService } from '../../services/auth-service.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Token } from '@angular/compiler';
+import { Router, RouterLink } from '@angular/router';
+import { skipUntil } from 'rxjs';
+import { CodeInputModule } from 'angular-code-input';
+
 @Component({
   selector: 'app-activate-account',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, CodeInputModule, RouterLink],
   templateUrl: './activate-account.component.html',
   styleUrl: './activate-account.component.scss'
 })
 export class ActivateAccountComponent {
 
-  constructor(private authService : AuthServiceService){}
+  constructor(private authService : AuthServiceService, private router: Router){}
 
-  inputValue: string = '';
+  message = '';
+  isOkay = true;
+  submitted = false;
 
-  sendToken(){
-      this.authService.confirm(this.inputValue).subscribe({
+  private confirmAccount(token: string) {
+    console.log(token)
+    this.authService.confirm(
+      token
+    ).subscribe({
       next: () => {
-        console.log("donne")
+        this.message = 'Your account has been successfully activated.\nNow you can proceed to login';
+        this.submitted = true;
       },
       error: () => {
-        console.log("err")
+        this.message = 'Token has been expired or invalid';
+        this.submitted = true;
+        this.isOkay = false;
       }
     });
   }
+
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+
+  protected readonly skipUntil = skipUntil;
 }
